9b08c46df267b392923c9db35c7835b79dca22a0
AliOnurSahiner <alionursahiner@aof.anadolu.edu.tr>
2022-12-18 22:37:49 +0300

    firstcommit

---
+++ webScraping/Controllers/ScrapingController.cs
@@ -0,0 +1,55 @@
+﻿using HtmlAgilityPack;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace webScraping.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ScrapingController : Controller
+    {
+        [HttpGet("webscraping")]
+        public List<string> Index()
+        {
+            string url = "https://www.goc.gov.tr";
+            var response = CallUrl(url).Result;
+            var linkList = ParseHtml(response);
+            return linkList;
+        }
+
+        private static async Task<string> CallUrl(string fullUrl)
+        {
+            HttpClient client = new HttpClient();
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls13;
+            client.DefaultRequestHeaders.Accept.Clear();
+            var response = client.GetStringAsync(fullUrl);
+            return await response;
+        }
+
+        private List<string> ParseHtml(string html)
+        {
+            HtmlDocument htmlDoc = new HtmlDocument();
+            htmlDoc.LoadHtml(html);
+            var programmerLinks = htmlDoc.DocumentNode.Descendants("ul")
+                    .Where(node => node.GetAttributeValue("class", "").Contains("dropdown-menu-right")).ToList();
+
+            List<string> wikiLink = new List<string>();
+
+            foreach (var link in programmerLinks[0].ChildNodes)
+            {
+                if(link.Name == "li")
+                {
+                    wikiLink.Add(link.InnerText);
+                }
+            }
+
+            return wikiLink;
+
+        }
+    }
+}
